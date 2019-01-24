b140a1c86ad98a358c29b3e34ad1c312af68316e
Vanio <jalmeida@golaw.com.br>
2019-01-24 09:53:43 -0200

    alteração de after em teste

--- test/main.ts
+++ test/main.ts
@@ -3,12 +3,12 @@ import { expect } from 'chai';
 
 import app from '../src/main';
 
+after(function(done: any){
+  app.close(done)
+})
+
 
 describe('GET / - a simple api endpoint', () => {
-
-  after(function(done: any){
-    app.close(done)
-  })
 
   it('Hello API Request', async () => {
     const result = await request(app).get('/');
